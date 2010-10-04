39e3f11a27717308f6c075df4aefcf9f382f4df0
jt1134 <jt1134@gmail.com>
2010-10-04 14:57:22 -0500

    add defconfig and build script

---
+++ build_kernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+make mrproper
+make clean
+
+make ARCH=arm yamaha_b5_defconfig
+make -j 8 CROSS_COMPILE=../arm-2009q3/bin/arm-none-linux-gnueabi- \
+	ARCH=arm HOSTCFLAGS="-g -O2"
+
+
