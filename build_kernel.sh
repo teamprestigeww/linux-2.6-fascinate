163da593dadaf6a46bb32063da28af517f617931
jt1134 <jt1134@gmail.com>
2010-10-06 16:51:44 -0500

    add files and update .gitignore and build_kernel.sh for update.zip

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,9 +3,14 @@
 make mrproper
 make clean
 rm -rf ../initramfs/.git
+rm update/*.zip update/kernel_update/zImage
 
 make ARCH=arm yamaha_b5_defconfig
 make -j 8 CROSS_COMPILE=../arm-2009q3/bin/arm-none-linux-gnueabi- \
 	ARCH=arm HOSTCFLAGS="-g -O2"
 
+cp arch/arm/boot/zImage update/kernel_update/zImage
+cd update
+zip -r kernel_update.zip . 
+cd ..
 
