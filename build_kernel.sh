802d625b22e98e1bf75b8290dca6e3a17e014087
jt1134 <jt1134@gmail.com>
2010-10-05 21:02:51 -0500

    add modules to build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,9 +3,14 @@
 make mrproper
 make clean
 rm -rf ../initramfs/.git
+cd ../modules
+make clean
 
+cd ../linux-2.6.29
 make ARCH=arm yamaha_b5_defconfig
 make -j 8 CROSS_COMPILE=../arm-2009q3/bin/arm-none-linux-gnueabi- \
 	ARCH=arm HOSTCFLAGS="-g -O2"
 
+cd ../modules
+make ARCH=arm CROSS_COMPILE=../arm-2009q3/bin/arm-none-linux-gnueabi-
 
