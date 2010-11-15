acf41cc1d130c2d81a72c962aed1211edb5af804
jt1134 <jt1134@gmail.com>
2010-11-14 23:45:59 -0600

    revert back to clock cpu speed, lower voltages a bit

--- arch/arm/plat-s5pc11x/max8998_consumer.c
+++ arch/arm/plat-s5pc11x/max8998_consumer.c
@@ -78,11 +78,11 @@ enum PMIC_VOLTAGE {
 static const unsigned int frequency_match_1GHZ[][4] = {
 /* frequency, Mathced VDD ARM voltage , Matched VDD INT*/
 #if 1
-        {1200000, 1325, 1100, 0},
-        {800000, 1200, 1100, 1},
-        {400000, 1050, 1100, 2},
-        {200000, 950, 1100, 4},
-        {100000, 950, 1000, 5},
+        {1000000, 1250, 1100, 0},
+        {800000, 1150, 1100, 1},
+        {400000, 1000, 1100, 2},
+        {200000, 900, 1000, 4},
+        {100000, 900, 1000, 5},
 #else //just for dvs test
         {1000000, 1250, 1100, 0},
         {800000, 1250, 1100, 1},
@@ -150,7 +150,7 @@ const unsigned int (*dvs_volt_table[2])[3] = {
 };
 
 static const unsigned int dvs_arm_voltage_set[][2] = {
-	{DVSARM1, 1300},
+	{DVSARM1, 1275},
 	{DVSARM2, 1200},
 	{DVSARM3, 1050},
 	{DVSARM4, 950},
